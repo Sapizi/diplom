691a064f5a3083ea8f58a6d932b32be05ebb3680
Sapizi <samokrutovandre261@gmail.com>
2026-01-14 23:10:57 +0500

    добавлкеик аналитики

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,6 +12,7 @@ export default function RootLayout({
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
